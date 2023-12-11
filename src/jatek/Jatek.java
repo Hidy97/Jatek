4ae8f56028ea31a3bd771a1bb2b7ce9fd2770afc
Hidy97 <csaladi.alexandra@gmail.com>
2023-12-11 11:05:44 +0100

    Használ metódusok megvannak, bonyolultabb kiir metódus átdolgozása

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -3,7 +3,23 @@ package jatek;
 public class Jatek {
 
     public static void main(String[] args) {
+        Karakter elf = new Karakter();
+        Targy fegyver1 = new Targy("Kard", "Egy ősiromok között talált rozsdás sz...");
+        Targy fegyver2 = new Targy("Kard", "A fény templomának ceremoniális kardja (meglepően éles...).");
+        Targy fizetoEszkoz1 = new Targy("Aranyérme", "Drááágasáááág");
+        Targy fizetoEszkoz2 = new Targy("Kristály", "Kék fényű ragyogó kristály, a kereskedők különösen szeretik");
         
+        elf.felvesz(fegyver2);
+        elf.felvesz(fegyver1);
+        elf.felvesz(fizetoEszkoz1);
+        elf.felvesz(fizetoEszkoz1);
+        elf.felvesz(fizetoEszkoz1);
+        elf.felvesz(fizetoEszkoz1);
+        elf.felvesz(fizetoEszkoz2);
+        elf.felvesz(fizetoEszkoz2);
+        
+        //System.out.println(elf.toString());
+        elf.kiir();
     }
     
 }
--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -1,44 +1,95 @@
 package jatek;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
  * @author csaladi.alexandra
  */
 public class Karakter {
-    
-    ArrayList<Targy> felszereles;
+
+    private ArrayList<Targy> felszereles;
     //String [] felszereles;
-    int eletero, ugyesseg, szerencse;
+    private int eletero, ugyesseg, szerencse;
+    private Random rnd = new Random();
 
-//    public Karakter(String[] felszereles, int eletero, int ugyesseg, int szerencse) {
-//        this.felszereles = felszereles;
-//        this.eletero = eletero;
-//        this.ugyesseg = ugyesseg;
-//        this.szerencse = szerencse;
-//    }
-
-    public Karakter(ArrayList<Targy> felszereles, int eletero, int ugyesseg, int szerencse) {
-        this.felszereles = felszereles;
-        this.eletero = dobas()+dobas()+12;
-        this.ugyesseg = dobas()+6;
-        this.szerencse = dobas()+6;
+    public Karakter() {
+        this.felszereles = new ArrayList<>();
+        this.eletero = dobas() + dobas() + 12;
+        this.ugyesseg = dobas() + 6;
+        this.szerencse = dobas() + 6;
     }
-    
-    private int dobas(){
-        int kocka = (int)Math.random()*(6-1)+1;
-        return kocka;
-    }   
-    
-    public Targy felvesz(Targy targy){
+
+    public ArrayList<Targy> getFelszereles() {
+        return felszereles;
+    }
+
+    public int getEletero() {
+        return eletero;
+    }
+
+    public int getUgyesseg() {
+        return ugyesseg;
+    }
+
+    public int getSzerencse() {
+        return szerencse;
+    }
+
+    private int dobas() {
+        return rnd.nextInt(6 + 1);
+    }
+
+    public Targy felvesz(Targy targy) {
         felszereles.add(targy);
         return targy;
+    }
+
+    public void hasznal(String nev) {
+        hasznal(nev, eletero);
+    }
+
+    public void hasznal(String nev, int darab) {
+        hasznal(nev, darab, true);
+    }
+
+    public void hasznal(String nev, int darab, boolean hasznalhato) {
+        int db = 0;
+        for (Targy felszerelese : felszereles) {
+            if (felszerelese.nev.equals(nev)) {
+                db++;
+                if (db < darab) {
+                    System.out.println("Nincs elég ebből a tárgyból!");
+                    hasznalhato = false;
+                } else {
+                    felszereles.remove(felszerelese);
+                    hasznalhato = true;
+                }
+            }
+        }
+    }
+
+    public void kiir() {
+        System.out.println("A tulajdonodban van: ");
+        int index = 0;
+        int darab = 0;
+        for (Targy felszerelese : felszereles) {
+            if (felszerelese.getNev().equals(felszereles.get(index).getNev()) ) {
+                darab++;
+                if (!(felszerelese.getNev().equals(felszereles.get(index).getNev())) && darab < 2) {
+                    System.out.print(felszerelese.nev + ": " + darab + ", ");
+
+                }
+                index++;
+            }
+        }
+        System.out.println("életerő: " + eletero + ", " + "ügyesség: " + ugyesseg + ", " + "szerencse: " + szerencse);
     }
 
     @Override
     public String toString() {
         return "Karakter{" + "felszereles=" + felszereles + ", eletero=" + eletero + ", ugyesseg=" + ugyesseg + ", szerencse=" + szerencse + '}';
     }
-        
+
 }
