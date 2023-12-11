ffe03b323e8fc811476c46e4ee0c0273d061021d
Hidy97 <csaladi.alexandra@gmail.com>
2023-12-11 08:51:40 +0100

    Kiegészítés felszereléssel

--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -1,10 +1,22 @@
 package jatek;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author csaladi.alexandra
  */
 public class Karakter {
-    int eletero, ugyesseg;
-    boolean szerencse;
+    
+    //ArrayList<String> felszereles;
+    String [] felszereles;
+    int eletero, ugyesseg, szerencse;
+
+    public Karakter(String[] felszereles, int eletero, int ugyesseg, int szerencse) {
+        this.felszereles = felszereles;
+        this.eletero = eletero;
+        this.ugyesseg = ugyesseg;
+        this.szerencse = szerencse;
+    } 
+    
 }
